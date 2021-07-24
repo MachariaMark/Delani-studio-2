816ae68c2a2a84d84e6bdcc12af6337e13081bee
mark nuchiri macharia <marknuchirimacharia@marks-MacBook-Air.local>
2021-07-25 00:32:14 +0300

    added portfolio section

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,29 @@ $(document).ready(function() {
     $("#prod-management-1").toggle();
     $("#prod-p").toggle();
   });
+  // portfolio section
+  $("#work4").hover(function(){
+    $(".work4").toggle(500);
+  });
+  $("#work3").hover(function(){
+    $(".work3").toggle(500);
+  });
+  $("#work2").hover(function(){
+    $(".work2").toggle(500);
+  });
+  $("#work1").hover(function(){
+    $(".work1").toggle(500);
+  });
+  $("#work5").hover(function(){
+    $(".work5").toggle(500);
+  });
+  $("#work6").hover(function(){
+    $(".work6").toggle(500);
+  });
+  $("#work7").hover(function(){
+    $(".work7").toggle(500);
+  });
+  $("#work8").hover(function(){
+    $(".work8").toggle(500);
+  });
+});
