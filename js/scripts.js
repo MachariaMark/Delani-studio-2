c36ef662f06c3cab32c17b1b170692fe4253511c
mark nuchiri macharia <marknuchirimacharia@marks-MacBook-Air.local>
2021-07-25 00:31:34 +0300

    added what we do section

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// what we do section
+$(document).ready(function() {
+  $("#design-1").click(function() {
+    $("#design-p").toggle();
+    $("#design-icon").toggle();
+  });
+  $("#development").click(function() {
+    $("#development-p").toggle();
+    $("#development-1").toggle();
+  });
+  $("#product-management").click(function() {
+    $("#prod-management-1").toggle();
+    $("#prod-p").toggle();
+  });
